6c024de773eb5b2468cf093f561534895941cc5e
Manny1995 <immanuelda@gmail.com>
2018-01-22 13:58:53 -0800

    added Process class, fcfs update

---
+++ src/classes/classes/Process.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std
+
+class Process {
+	public:
+		float arrivalTime;
+		float duration;
+		int priority;
+		Process(float, float, int);
+		Process();
+
+};
