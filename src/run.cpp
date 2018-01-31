63cde3e4a68375ab1916472afb8c7a7585816056
Manny1995 <immanuelda@gmail.com>
2018-01-30 19:02:58 -0800

    added command line support (makes it cleaner when we demo :)

--- src/run.cpp
+++ src/run.cpp
@@ -18,19 +18,61 @@
 using namespace std;
 
 
+void help() {
+		cout << "all 	- run all 6 algorithms" << endl;
+		cout << "fcfs 	- first come first serve" << endl;
+		cout << "sjf 	- shortest job first" << endl;
+		cout << "str 	- shortest time remaining" << endl;
+		cout << "rr 	- round robin" << endl;
+		cout << "hpf1	- highest priority first non-preemptive" << endl;
+		cout << "hpf2	- highest priority first preemptive" << endl;
+}
 
 int main(int argc, char* argv[]) {
 
+	if (argc != 2) {
+		cout << "Please enter an argument:" << endl;
+		help();
+		return -1;
+	}
+
 	vector< vector<Process *> > data = generate::generateSimulationData();
 
+	string choice = string(argv[1]);
 
-	// Some of the models directly modify the simulated data in their implementation.
-	// To avoid these changes propogating to the next simulation, generate::getCopiedData
-	// creates a **deep copy** of the generated data.
-	fcfsDriver(generate::getCopiedData(data));
-	sjfDriver(generate::getCopiedData(data));
-	strDriver(generate::getCopiedData(data));
-	rrDriver(generate::getCopiedData(data));
+	if (choice == "all") {
+
+		// Some of the models directly modify the simulated data in their implementation.
+		// To avoid these changes propogating to the next simulation, generate::getCopiedData
+		// creates a **deep copy** of the generated data.
+		fcfsDriver(generate::getCopiedData(data));
+		sjfDriver(generate::getCopiedData(data));
+		strDriver(generate::getCopiedData(data));
+		rrDriver(generate::getCopiedData(data));
+	}
+	else if (choice == "fcfs") {
+		fcfsDriver(generate::getCopiedData(data));
+	}
+	else if (choice == "sjf") {
+		sjfDriver(generate::getCopiedData(data));
+	}
+	else if (choice == "str") {
+		strDriver(generate::getCopiedData(data));
+	}
+	else if (choice == "rr") {
+		rrDriver(generate::getCopiedData(data));
+	}
+	else if (choice == "hpf1") {
+
+	}
+	else if (choice == "hpf2") {
+
+	}
+	else {
+		cout << "Invalid argument, please try again!" << endl;
+		help();
+	}
 
 	return 1;
+
 }
